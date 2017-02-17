03870b34c40b6bea6a73d9cf3ea629364023bec7
eum123 <eummanjin@gmail.com>
2017-02-17 17:22:25 +0900

    .

---
+++ src/timecomplexity/PermMissingElem.java
@@ -0,0 +1,72 @@
+package timecomplexity;
+
+import java.util.Arrays;
+
+/**
+ * <pre>
+ * A zero-indexed array A consisting of N different integers is given. The array
+ * contains integers in the range [1..(N + 1)], which means that exactly one
+ * element is missing.
+ * 
+ * Your goal is to find that missing element.
+ * 
+ * Write a function:
+ * 
+ * class Solution { public int solution(int[] A); }
+ * 
+ * that, given a zero-indexed array A, returns the value of the missing element.
+ * 
+ * For example, given array A such that:
+ * 
+ * A[0] = 2 A[1] = 3 A[2] = 1 A[3] = 5 the function should return 4, as it is
+ * the missing element.
+ * 
+ * Assume that:
+ * 
+ * N is an integer within the range [0..100,000]; the elements of A are all
+ * distinct; each element of array A is an integer within the range [1..(N +
+ * 1)]. Complexity:
+ * 
+ * expected worst-case time complexity is O(N); expected worst-case space
+ * complexity is O(1), beyond input storage (not counting the storage required
+ * for input arguments). Elements of input arrays can be modified.
+ * </pre>
+ * 
+ * @author jin
+ *
+ */
+public class PermMissingElem {
+	public int solution(int[] A) {
+		Arrays.sort(A);
+		
+		int value = 1;
+		
+		
+		for(int i=0 ;i<A.length ;i++) {
+			value = i+1;
+			if(A[i] != i+1) {
+				break;
+			} else {
+				if(i == A.length -1 ) {
+					value = A.length+1;
+				}
+			}
+		}
+		
+		return value;
+	}
+	
+	public static void main(String[] args) {
+		PermMissingElem t = new PermMissingElem();
+		System.out.println(t.solution(new int[]{1}));
+		
+		System.out.println(t.solution(new int[]{}));
+		
+		//
+		System.out.println(t.solution(new int[]{2,3,4,5}));
+		System.out.println(t.solution(new int[]{1,2,3,4,5,7}));
+		
+		//
+		System.out.println(t.solution(new int[]{1,2}));
+	}
+}
